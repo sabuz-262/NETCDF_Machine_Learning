0fa993ddff0a8d0150fc874aab226ff411ab2d1f
mfaruk <omar.faruk.buet@gmail.com>
2018-12-21 03:16:28 -0800

    Create unitTest.py

---
+++ unitTest.py
@@ -0,0 +1,36 @@
+import unittest
+import NETCDF
+from sys import platform as sys_pf
+if sys_pf == 'darwin':
+    import matplotlib
+    matplotlib.use("TkAgg")
+import Constants
+
+
+class NETSCDFTest(unittest.TestCase):
+    # Returns True or False.
+    def test_read_all_file(self):
+        dataframe = NETCDF.read_all_files()
+        assert len(dataframe) == 1440*9
+
+    def test_resample(self):
+        dataframe = NETCDF.read_all_files()
+        dataframe2 = NETCDF.rename_column(dataframe)
+        assert len(dataframe) == len(dataframe2)*5
+
+    def test_resample_rename(self):
+        dataframe = NETCDF.read_all_files()
+        dataframe = NETCDF.rename_column(dataframe)
+        dataframe = NETCDF.rename_column(dataframe)
+        assert dataframe.shape[1] == 4
+
+    def test_store_resample_data(self):
+        dataframe = NETCDF.read_all_files()
+        dataframe = NETCDF.rename_column(dataframe)
+        dataframe = NETCDF.rename_column(dataframe)
+        NETCDF.store_resample_data(dataframe)
+        dataframe2 = NETCDF.read_cdf_file(Constants.output_file_name)
+        assert len(dataframe) == len(dataframe2)
+
+if __name__ == '__main__':
+    unittest.main()
